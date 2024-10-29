fb88d284582d092f530f862edbf58d9dda3e22f2
Henry Huynh <huynh.vu2201@gmail.com>
2024-10-28 23:25:11 -0500

    optional step

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 import { puppyList } from "./data.js"
 import { useState } from "react"
-
+import './App.css'
+import './index.css'
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  console.log("puppyList: ", puppyList);
+  const handleClick = (puppyId) => {
+    setFeatPupId(puppyId);
+  };
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
